315753aa46bd6dd8403e8484b5662e9b82354359
getinthere <getinthere@naver.com>
2024-02-05 11:29:37 +0900

    board 데이터 초기화 - 게시글 쓰기보다, 게시글 목록보기를 먼저하려고

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Table(name="board_tb")
+@Data
+@Entity // 테이블 생성하기 위해 필요한 어노테이션
+public class Board { // User 1 -> Board N
+    @Id // PK 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 전략
+    private int id;
+    private String title;
+    private String content;
+
+    private int userId; // 테이블에 만들어 질때 user_id
+
+    private LocalDateTime createdAt;
+}
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,7 @@
 insert into user_tb(username, password, email, created_at) values('ssar', '1234', 'ssar@nate.com', now());
 insert into user_tb(username, password, email, created_at) values('cos', '1234', 'cos@nate.com', now());
+
+insert into board_tb(title, content, user_id, created_at) values('제목1', '내용1', 1, now());
+insert into board_tb(title, content, user_id, created_at) values('제목2', '내용2', 1, now());
+insert into board_tb(title, content, user_id, created_at) values('제목3', '내용3', 1, now());
+insert into board_tb(title, content, user_id, created_at) values('제목4', '내용4', 2, now());
