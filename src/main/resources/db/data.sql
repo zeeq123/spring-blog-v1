a7df90f3b557e3f49f0b539fb3bd84a9b3491dcc
김정수 <zeeq125@gmail.com>
2024-02-16 11:09:04 +0900

    더미 데이터

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -5,3 +5,8 @@ insert into board_tb(title, content, user_id, created_at) values('제목1', '내
 insert into board_tb(title, content, user_id, created_at) values('제목2', '내용2', 1, now());
 insert into board_tb(title, content, user_id, created_at) values('제목3', '내용3', 1, now());
 insert into board_tb(title, content, user_id, created_at) values('제목4', '내용4', 2, now());
+
+insert into reply_tb(comment, board_id, user_id, created_at) values('댓글1', 1, 1, now());
+insert into reply_tb(comment, board_id, user_id, created_at) values('댓글2', 4, 1, now());
+insert into reply_tb(comment, board_id, user_id, created_at) values('댓글3', 4, 1, now());
+insert into reply_tb(comment, board_id, user_id, created_at) values('댓글4', 4, 2, now());
